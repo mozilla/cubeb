4bc13035b143ac299a58b8467b87212fc1f5958c
Paul Adenot <paul@paul.cx>
2014-02-26 19:48:20 +0100

    Some more change to support testing
    - put the `delay` function in the header to make it easy to write gecko tests
    - add a test to test new cubeb API function
    - update tests so they pass on every platform
    - fix the bustage caused by the previous patch

--- test/common.h
+++ test/common.h
@@ -5,7 +5,24 @@
  * accompanying file LICENSE for details.
  */
 
-void delay(unsigned int ms);
+#if defined( _WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void delay(unsigned int ms)
+{
+#if defined(_WIN32)
+	Sleep(ms);
+#else
+	sleep(ms / 1000);
+	usleep(ms % 1000 * 1000);
+#endif
+}
 
 #if !defined(M_PI)
 #define M_PI 3.14159265358979323846
--- test/test_audio.c
+++ test/test_audio.c
@@ -7,6 +7,9 @@
 
 /* libcubeb api/function exhaustive test. Plays a series of tones in different
  * conditions. */
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
 #define _XOPEN_SOURCE 500
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,7 +42,7 @@ typedef struct {
 
 synth_state* synth_create(int num_channels, float sample_rate)
 {
-  synth_state* synth = malloc(sizeof(synth_state));
+  synth_state* synth = (synth_state *) malloc(sizeof(synth_state));
   for(int i=0;i < MAX_NUM_CHANNELS;++i)
     synth->phase[i] = 0.0f;
   synth->num_channels = num_channels;
@@ -142,8 +145,6 @@ cleanup:
 
 int main(int argc, char *argv[])
 {
-  int ret;
-
   int channel_values[] = {
     1,
     2,
