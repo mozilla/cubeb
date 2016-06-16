4d2d6e11b40f96c78b3abc503dd996780d84ebc6
Alex Chronopoulos <achronop@gmail.com>
2016-06-16 10:26:01 +0100

    Review comments for cubeb-sles.h

--- src/cubeb-sles.h
+++ src/cubeb-sles.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Mozilla Foundation
+ * Copyright © 2016 Mozilla Foundation
  *
  * This program is made available under an ISC-style license.  See the
  * accompanying file LICENSE for details.
@@ -15,31 +15,29 @@ cubeb_get_sles_engine(SLObjectItf *           pEngine,
                       const SLEngineOption *  pEngineOptions,
                       SLuint32                numInterfaces,
                       const SLInterfaceID *   pInterfaceIds,
-                      const SLboolean *       pInterfaceRequired) {
-    SLresult lRes;
-    lRes = slCreateEngine(pEngine,
-                          numOptions,
-                          pEngineOptions,
-                          numInterfaces,
-                          pInterfaceIds,
-                          pInterfaceRequired);
-    return lRes;
+                      const SLboolean *       pInterfaceRequired)
+{
+  return slCreateEngine(pEngine,
+                        numOptions,
+                        pEngineOptions,
+                        numInterfaces,
+                        pInterfaceIds,
+                        pInterfaceRequired);
 }
 
-static void cubeb_destroy_sles_engine(SLObjectItf * self) {
-    if (*self != NULL) {
-        (**self)->Destroy(*self);
-        *self = NULL;
-    }
+static void
+cubeb_destroy_sles_engine(SLObjectItf * self)
+{
+  if (*self != NULL) {
+      (**self)->Destroy(*self);
+      *self = NULL;
+  }
 }
 
-/* Only synchronous operation is supported, as if the second
-   parameter was FALSE. */
-static SLresult cubeb_realize_sles_engine(SLObjectItf self) {
-    SLresult lRes = (*self)->Realize(self,SL_BOOLEAN_FALSE);
-    if (lRes != SL_RESULT_SUCCESS) {
-        return lRes;
-    }
+static SLresult
+cubeb_realize_sles_engine(SLObjectItf self)
+{
+  return (*self)->Realize(self, SL_BOOLEAN_FALSE);
 }
 
 #endif
