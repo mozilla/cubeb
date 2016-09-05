mozilla/cubeb

commit 3148466d261f411bb7ef9464dcd8a95373f0dafe

When we're draining, we're not advancing the synthetized clock so the delay can go negative.

src/cubeb_wasapi.cpp
@@ -1021,7 +1021,7 @@ current_stream_delay(cubeb_stream * stm)
   double cur_pos = static_cast<double>(pos) / freq;
   double max_pos = static_cast<double>(stm->frames_written)  / stm->output_mix_params.rate;
   double delay = max_pos - cur_pos;
-  XASSERT(delay >= 0);
+  XASSERT(delay >= 0 || stm->draining);
 
   return delay;
 }

commit 5e57da1bf79d8500aed62ac8e511cb5291307161

wasapi: Fix drain regression hidden by 3148466d.

src/cubeb_wasapi.cpp
@@ -760,8 +760,10 @@ refill_callback_output(cubeb_stream * stm)
                     0,
                     output_buffer,
                     output_frames);
+  XASSERT(got >= 0);
+  XASSERT(got == output_frames || stm->draining);
 
-  hr = stm->render_client->ReleaseBuffer(output_frames, 0);
+  hr = stm->render_client->ReleaseBuffer(got, 0);
   if (FAILED(hr)) {
     LOG("failed to release buffer: %x\n", hr);
     return false;
@@ -1022,7 +1024,7 @@ current_stream_delay(cubeb_stream * stm)
   double cur_pos = static_cast<double>(pos) / freq;
   double max_pos = static_cast<double>(stm->frames_written)  / stm->output_mix_params.rate;
   double delay = max_pos - cur_pos;
-  XASSERT(delay >= 0 || stm->draining);
+  XASSERT(delay >= 0);
 
   return delay;
 }