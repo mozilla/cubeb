e556eb948afc8473e69c00365664118f49581b8f
Paul Adenot <paul@paul.cx>
2016-11-29 17:10:40 +0100

    Allow the ringbuffer to contain any type.

--- src/cubeb_ringbuffer.h
+++ src/cubeb_ringbuffer.h
@@ -105,9 +105,6 @@ public:
     /* One more element to distinguish from emtpy and full buffer. */
     , capacity_(capacity + 1)
   {
-    static_assert(std::is_trivially_copyable<T>::value,
-                  "ring_buffer_base requires trivial type");
-
     assert(storage_capacity() <
            std::numeric_limits<RingBufferIndex>::max() &&
            "buffer to large for the type of index used.");
@@ -170,11 +167,11 @@ public:
     int second_part = to_write - first_part;
 
     if (elements) {
-      PodCopy(data_.get() + wr_idx, elements, first_part);
-      PodCopy(data_.get(), elements + first_part, second_part);
+      Copy(data_.get() + wr_idx, elements, first_part);
+      Copy(data_.get(), elements + first_part, second_part);
     } else {
-      PodZero(data_.get() + wr_idx, first_part);
-      PodZero(data_.get(), second_part);
+      ConstructDefault(data_.get() + wr_idx, first_part);
+      ConstructDefault(data_.get(), second_part);
     }
 
     increment_index(wr_idx, to_write);
@@ -209,8 +206,8 @@ public:
     int second_part = to_read - first_part;
 
     if (elements) {
-      PodCopy(elements, data_.get() + rd_idx, first_part); 
-      PodCopy(elements + first_part, data_.get(), second_part);
+      Copy(elements, data_.get() + rd_idx, first_part);
+      Copy(elements + first_part, data_.get(), second_part);
     }
 
     increment_index(rd_idx, to_read);
