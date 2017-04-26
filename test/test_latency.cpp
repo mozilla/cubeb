f401f7ef69d2d66415fb3d4ad6ebc079ba02b665
Chun-Min Chang <chun.m.chang@gmail.com>
2017-04-26 16:08:19 +0800

    cubeb_get_preferred_channel_layout should be verified in test_sanity instead of test_latency

--- test/test_latency.cpp
+++ test/test_latency.cpp
@@ -11,7 +11,6 @@ TEST(cubeb, latency)
   uint32_t max_channels;
   uint32_t preferred_rate;
   uint32_t latency_frames;
-  cubeb_channel_layout layout;
 
   r = common_init(&ctx, "Cubeb audio test");
   ASSERT_EQ(r, CUBEB_OK);
@@ -31,15 +30,11 @@ TEST(cubeb, latency)
     ASSERT_GT(preferred_rate, 0u);
   }
 
-  r = cubeb_get_preferred_channel_layout(ctx, &layout);
-  ASSERT_TRUE(r == CUBEB_OK || r == CUBEB_ERROR_NOT_SUPPORTED ||
-              (r == CUBEB_ERROR && layout == CUBEB_LAYOUT_UNDEFINED));
-
   cubeb_stream_params params = {
     CUBEB_SAMPLE_FLOAT32NE,
     preferred_rate,
     max_channels,
-    (r == CUBEB_OK) ? layout : CUBEB_LAYOUT_UNDEFINED
+    CUBEB_LAYOUT_UNDEFINED
 #if defined(__ANDROID__)
     , CUBEB_STREAM_TYPE_MUSIC
 #endif
