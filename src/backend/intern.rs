a386d91d7f34e6d3a949ee4fa44e848e29634587
Matthew Gregan <kinetik@flim.org>
2017-09-05 14:36:11 +1200

    backend: Add test for Intern.

--- src/backend/intern.rs
+++ src/backend/intern.rs
@@ -30,3 +30,29 @@ impl Intern {
         self.vec.last().unwrap().as_ptr()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::CStr;
+    use super::Intern;
+
+    #[test]
+    fn intern() {
+        fn cstr(str: &[u8]) -> &CStr {
+            CStr::from_bytes_with_nul(str).unwrap()
+        }
+
+        let mut intern = Intern::new();
+
+        let foo_ptr = intern.add(cstr(b"foo\0"));
+        let bar_ptr = intern.add(cstr(b"bar\0"));
+        assert!(!foo_ptr.is_null());
+        assert!(!bar_ptr.is_null());
+        assert!(foo_ptr != bar_ptr);
+
+        assert!(foo_ptr == intern.add(cstr(b"foo\0")));
+        assert!(foo_ptr != intern.add(cstr(b"fo\0")));
+        assert!(foo_ptr != intern.add(cstr(b"fool\0")));
+        assert!(foo_ptr != intern.add(cstr(b"not foo\0")));
+    }
+}
