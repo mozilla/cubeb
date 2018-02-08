1fb26752cd2723c76b84807355236da8c4a630a8
achronop <achronop@gmail.com>
2018-02-08 16:56:30 +0200

    wasapi: reset stream on invalidated device error.

--- src/cubeb_wasapi.cpp
+++ src/cubeb_wasapi.cpp
@@ -615,6 +615,8 @@ refill(cubeb_stream * stm, void * input_buffer, long input_frames_count,
   return out_frames;
 }
 
+int wasapi_stream_reset_default_device(cubeb_stream * stm);
+
 /* This helper grabs all the frames available from a capture client, put them in
  * linear_input_buffer. linear_input_buffer should be cleared before the
  * callback exits. This helper does not work with exclusive mode streams. */
@@ -637,6 +639,14 @@ bool get_input_buffer(cubeb_stream * stm)
   for (hr = stm->capture_client->GetNextPacketSize(&next);
        next > 0;
        hr = stm->capture_client->GetNextPacketSize(&next)) {
+
+    if (hr == AUDCLNT_E_DEVICE_INVALIDATED) {
+      // Application can recover from that error. More info
+      // https://msdn.microsoft.com/en-us/library/windows/desktop/dd316605(v=vs.85).aspx
+      LOG("Device invalidated error, reset deafult device");
+      wasapi_stream_reset_default_device(stm);
+      return true;
+    }
 
     if (FAILED(hr)) {
       LOG("cannot get next packet size: %lx", hr);
@@ -713,10 +723,20 @@ bool get_output_buffer(cubeb_stream * stm, void *& buffer, size_t & frame_count)
   XASSERT(has_output(stm));
 
   hr = stm->output_client->GetCurrentPadding(&padding_out);
+
+  if (hr == AUDCLNT_E_DEVICE_INVALIDATED) {
+      // Application can recover from that error. More info
+      // https://msdn.microsoft.com/en-us/library/windows/desktop/dd316605(v=vs.85).aspx
+      LOG("Device invalidated error, reset deafult device");
+      wasapi_stream_reset_default_device(stm);
+      return true;
+  }
+
   if (FAILED(hr)) {
     LOG("Failed to get padding: %lx", hr);
     return false;
   }
+
   XASSERT(padding_out <= stm->output_buffer_frame_count);
 
   if (stm->draining) {
