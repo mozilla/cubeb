mozilla/cubeb

commit a7b7cdbeeb79191fbf9c8dbe3934d9d1c5531067

sndio: test if sndio works in sndio_init
This avoids issues when uses have libsndio installed but are not using
it, also prefer sndio devices i.e. snd/0 by default on platforms other
than OpenBSD. This way cubebs alsa backend can be used insted of using
libsndio code which will use alsa directly, but with no benefit over
cubebs implementation.

src/cubeb_sndio.c
@@ -125,6 +125,23 @@ s16_to_float(void *ptr, long nsamp)
     *(--dst) = (1. / 32768) * *(--src);
 }
 
+static const char *
+sndio_get_device()
+{
+#ifndef __OpenBSD__
+  /*
+   * On other platforms default to sndio devices,
+   * so cubebs other backends can be used instead.
+   */
+  const char *dev = getenv("AUDIODEVICE");
+  if (dev == NULL || *dev == '\0')
+	return "snd/0";
+  return dev;
+#else
+  return SIO_DEVANY;
+#endif
+}
+
 static void
 sndio_onmove(void *arg, int delta)
 {
@@ -288,6 +305,7 @@ sndio_mainloop(void *arg)
 sndio_init(cubeb **context, char const *context_name)
 {
   void * libsndio = NULL;
+  struct sio_hdl *hdl;
 
   assert(context);
 
@@ -313,6 +331,13 @@ sndio_init(cubeb **context, char const *context_name)
   LIBSNDIO_API_VISIT(LOAD);
 #undef LOAD
 #endif
+
+  /* test if sndio works */
+  hdl = WRAP(sio_open)(sndio_get_device(), SIO_PLAY, 1);
+  if (hdl == NULL) {
+    return CUBEB_ERROR;
+  }
+  WRAP(sio_close)(hdl);
 
   DPR("sndio_init(%s)\n", context_name);
   *context = malloc(sizeof(**context));
@@ -388,7 +413,7 @@ sndio_stream_init(cubeb * context,
     goto err;
   }
   s->context = context;
-  s->hdl = WRAP(sio_open)(NULL, s->mode, 1);
+  s->hdl = WRAP(sio_open)(sndio_get_device(), s->mode, 1);
   if (s->hdl == NULL) {
     DPR("sndio_stream_init(), sio_open() failed\n");
     goto err;

commit c0a9ea4cc6e2b28af55520b16c5c9149b33cb430

Always use SIO_DEVANY on FreeBSD/DragonFly
Forcing cubeb to snd/0 is entirely inappropriate for FreeBSD where
using the sndio backend without a running sndiod is completely ok
and does not suffer from the same drawbacks (blocking the audio
device) as ALSA on Linux.

Using SIO_DEVANY (i.e., "default") will cause libsndio to open the
default device /dev/dsp on FreeBSD, so libsndio can be used without
running sndiod or extra configuration like setting the AUDIODEVICE
environment variable.

Regressed by a7b7cdbeeb79191fbf9c8dbe3934d9d1c5531067.

Signed-off-by: Tobias Kortkamp <t@tobik.me>

src/cubeb_sndio.c
@@ -128,7 +128,7 @@ s16_to_float(void *ptr, long nsamp)
 static const char *
 sndio_get_device()
 {
-#ifndef __OpenBSD__
+#ifdef __linux__
   /*
    * On other platforms default to sndio devices,
    * so cubebs other backends can be used instead.