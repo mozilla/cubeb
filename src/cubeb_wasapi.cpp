1a7ab74fcca036a540364bedaef769fe2d43214b
Matthew Gregan <kinetik@flim.org>
2026-08-11 12:59:50 +1200

    wasapi: Mix output when channel counts differ

--- src/cubeb_wasapi.cpp
+++ src/cubeb_wasapi.cpp
@@ -2838,7 +2838,8 @@ setup_wasapi_stream(cubeb_stream * stm)
 
   // Create output mixer.
   if (has_output(stm) &&
-      stm->output_mix_params.layout != stm->output_stream_params.layout) {
+      (stm->output_mix_params.layout != stm->output_stream_params.layout ||
+       stm->output_mix_params.channels != stm->output_stream_params.channels)) {
     if (stm->output_mix_params.layout == CUBEB_LAYOUT_UNDEFINED) {
       LOG("Output stream using undefined layout! Any mixing may be "
           "unpredictable!\n");
